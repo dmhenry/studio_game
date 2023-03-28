8b70df46e57917d57e7437f81c46ffcf0db7e045
dmhenry <davermont@gmail.com>
2023-03-28 16:42:27 -0400

    Complete section 3

--- studio_game.rb
+++ studio_game.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-name1 = 'Larry'
+name1 = "Larry"
+name2 = "Curly"
+name3 = "Moe"
 health1 = 60
 
-puts name1 + '\'s health is ' + health1.to_s
+puts "#{name1}'s health is #{health1 / 9.0}"
+
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
